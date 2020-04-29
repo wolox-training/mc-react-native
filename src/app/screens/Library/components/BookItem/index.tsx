295975539425fb1888ab5d711cc60bd13820d4cf
mcolombo87 <mcolombo87@gmail.com>
2020-04-29 19:56:07 -0300

    New Router add to this project

--- src/app/screens/Library/components/BookItem/index.tsx
+++ src/app/screens/Library/components/BookItem/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, TouchableOpacity} from 'react-native';
 import styles from './styles';
 import {imageMissing} from '../../../../../constants/constants';
+import {useNavigation} from '@react-navigation/native';
 
 interface Props {
   title: string;
@@ -10,14 +11,19 @@ interface Props {
 }
 
 function BookItem({title, author, uri}: Props) {
+  const navigation = useNavigation();
   return (
-    <View style={styles.bookContainer}>
+    <TouchableOpacity
+      style={styles.bookContainer}
+      onPress={() => {
+        navigation.navigate('BookDetail');
+      }}>
       <Image source={uri ? {uri} : imageMissing} style={styles.cover} />
       <View style={styles.description}>
         <Text style={styles.title}>{title}</Text>
         <Text style={styles.author}>{author}</Text>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
 
