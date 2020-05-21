da7d47317a6e661e78ed6e345c2e8dbda347c55c
mcolombo87 <mcolombo87@gmail.com>
2020-05-21 16:35:36 -0300

    move LocalStorage to services

--- babel.config.js
+++ babel.config.js
@@ -16,7 +16,7 @@ module.exports = {
           '@services': './src/services',
           '@utils': './src/utils',
           '@components': './src/app/components',
-          '@localStore': './src/localStorage'
+          '@localStore': './src/services/localStorage'
         }
       }
     ]
